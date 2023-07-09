8999c21f3493531296f03e1d0e6b42708a6de196
nishiki-7 <hika.create@gmail.com>
2023-07-09 11:35:47 +0900

    LINEクローンチュートリアル完成

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import SignOut from './SignOut'
-import {db} from "../firebase.js"
+import {db, auth} from "../firebase.js"
+import SendMessage from './SendMessage';
 
 const Line = () => {
-    const [messages, setMessages] = useState();
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         db.collection("messages")
@@ -18,6 +19,21 @@ const Line = () => {
         <div>
             {console.log(messages)}
         <SignOut />
+        <div className="msgs">
+            {messages.map(({id, text, photoURL, uid}) => (
+                <div>
+                    <div 
+                    key={id}
+                    className={`msg ${
+                        uid === auth.currentUser.uid ? "sent" : "received"}`}
+                    >
+                        <img src={photoURL} alt="" />
+                        <p>{text}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        <SendMessage />
         </div>
     )
 }
